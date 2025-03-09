5051fe8e6a1fce1eeff89586f2ad60f774151fb4
Monu_2114 <monu.nanabala@gmail.com>
2025-03-09 19:28:01 +0530

    initial commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <div className="">Hii I am Monisha</div>;
+}
---
+++ src/app/projects/page.jsx
@@ -0,0 +1,10 @@
+export default function projects() {
+  return (
+    <div>
+      <div>Dumbcharades</div>
+      <div>Memory Card</div>
+      <div>Role Based Access</div>
+      <div></div>
+    </div>
+  );
+}
